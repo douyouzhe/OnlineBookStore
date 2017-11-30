2a2667ae61e1e0274a383f3320ea86aced470015
douyouzhe <douyouzhe@gmail.com>
2017-11-29 21:07:03 -0500

    1

--- src/com/obs/client/AddToCartServlet.java
+++ src/com/obs/client/AddToCartServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddToCartServlet extends HttpServlet{
 
@@ -28,7 +29,29 @@ public class AddToCartServlet extends HttpServlet{
                 req.getSession().setAttribute("cart",cart);
             }
             service.addBooktoCart(cart,book);
-            return;
+//            PrintWriter out = resp.getWriter(  );
+//            resp.setContentType("text/html");
+//            System.out.println(req.getContextPath());
+//            out.println("<P>Book added");
+//            out.println("<br><br>");
+//            out.println("<P><a href='"+req.getContextPath()+"/client/head.jsp> Continue shopping </a>");
+//            out.println("<br><br>");
+//            out.println("<P><a href='"+req.getContextPath()+"/client/listCart.jsp> Check out </a>");
+
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hola</title>");
+            out.println("</head>");
+            out.println("<body bgcolor=\"white\">");
+            out.println("<p>Book added</p>");
+            out.println("<p><a href="+req.getContextPath()+"/client/head.jsp> Continue shopping </a></p>");
+            out.println("<p><a href="+req.getContextPath()+"/client/listCart.jsp> Check out </a></p>");
+
+            out.println("</body>");
+            out.println("</html>");
 
 
         }catch(Exception e){
