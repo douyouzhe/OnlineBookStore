f3f09499a69e8815dbad7d97fddfe967c9d1b1e4
douyouzhe <douyouzhe@gmail.com>
2017-12-28 17:15:39 -0500

    1

--- src/com/obs/client/UserProfileServlet.java
+++ src/com/obs/client/UserProfileServlet.java
@@ -25,34 +25,48 @@ public class UserProfileServlet extends HttpServlet{
         List<Book> ls= service.getAllBook(orderId);
         System.out.print(ls.size());
         req.setAttribute("BookList",ls);
+        User user = (User) req.getSession().getAttribute("user");
+        req.setAttribute("username", user.getUsername());
+        req.setAttribute("address", user.getAddress());
+        req.setAttribute("tel", user.getTel());
+        req.setAttribute("email", user.getEmail());
+        req.setAttribute("id", user.getId());
+        service = new BusinessServiceImpl();
+        List<Order> order;
+        order = service.listOrderForUser(user.getId());
+        req.setAttribute("order", order);
         req.getRequestDispatcher("client/userProfile.jsp").forward(req,resp);
-
-            }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method=req.getParameter("method");
-
-
-            User user = (User) req.getSession().getAttribute("user");
-            if (user != null) {
-                req.setAttribute("username", user.getUsername());
-                req.setAttribute("address", user.getAddress());
-                req.setAttribute("tel", user.getTel());
-                req.setAttribute("email", user.getEmail());
-                req.setAttribute("id", user.getId());
-
-                BusinessServiceImpl service = new BusinessServiceImpl();
-                List<Order> order;
-                order = service.listOrderForUser(user.getId());
-                req.setAttribute("order", order);
-
-
-                req.getRequestDispatcher("client/userProfile.jsp").forward(req, resp);
-            } else {
-                req.setAttribute("message", "Log in first!");
-                req.getRequestDispatcher("/message.jsp").forward(req, resp);
-            }
+        if(method.equals("displayUserInfo")){
+            displayUserInfo(req,resp);
         }
+        else if(method.equals("displayOrderInfo")){
+            listOrders(req,resp);
+        }
+
+    }
+
+    private void displayUserInfo(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("user");
+        if (user != null) {
+            req.setAttribute("username", user.getUsername());
+            req.setAttribute("address", user.getAddress());
+            req.setAttribute("tel", user.getTel());
+            req.setAttribute("email", user.getEmail());
+            req.setAttribute("id", user.getId());
+            BusinessServiceImpl service = new BusinessServiceImpl();
+            List<Order> order;
+            order = service.listOrderForUser(user.getId());
+            req.setAttribute("order", order);
+            req.getRequestDispatcher("client/userProfile.jsp").forward(req, resp);
+        } else {
+            req.setAttribute("message", "Log in first!");
+            req.getRequestDispatcher("/message.jsp").forward(req, resp);
+        }
+    }
 
 }
