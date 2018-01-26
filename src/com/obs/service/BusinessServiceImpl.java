678477861b3febfa1da411b47d12a787359d15f2
douyouzhe <douyouzhe@gmail.com>
2018-01-25 19:16:28 -0500

    1

--- src/com/obs/service/BusinessServiceImpl.java
+++ src/com/obs/service/BusinessServiceImpl.java
@@ -164,14 +164,7 @@ public class BusinessServiceImpl implements BusinessService {
             {
                 int euclideanDistance = 0;
                 orderAmount = getInfoMatrix(ls, oibc.getId());
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 0c9af94b9389b6533d5dc8e6415a35daddb37617
-=======
-
->>>>>>> 0c9af94b9389b6533d5dc8e6415a35daddb37617
                 for (int i = 0; i < 10; i++) {
                     int tmp = (orderAmount[i] - orderAmountUser[i]) * (orderAmount[i] - orderAmountUser[i]);
                     euclideanDistance += tmp;
